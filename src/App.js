18b24451c253669ebab35b678058233cde4d68b3
Alex Carvajal <alexcarvajal@unicauca.edu.co>
2023-02-13 00:24:22 -0500

    navbar y slide

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import logo from './Logo.png';
 import './App.css';
 import MiComponent from './MiComponent';
-import { MiComponent2 } from './MiComponent2';
-import {MiComponent3} from './MiComponent3';
-import { EventosComponente } from './EventosComponente';
 import Navbar from './Navbar';
 
 function App() {
-    const ficha_m={
-      altura: "187cm",
-      grupo: "H+",
-      estado: "Bueno",
-      alergias: "Ninguna"
-    }
 
   return (
-
     <div className="App">
       <Navbar></Navbar>
       <header className="App-header">
@@ -24,18 +14,13 @@ function App() {
           Eol Solutions
         </p></h1> 
        {/*Cargar component*/}
-
       <div className='Componentes'>
-      <hr/>
-      <EventosComponente />
-      <hr/>
+      {/*<EventosComponente />*/}
        <MiComponent/>
-       <hr/>
-       <MiComponent2/>
-       <hr/>
-       <MiComponent3 nombre="alex" apellidos="Carvajal" ficha= {ficha_m} />
+       {/*<MiComponent2/>+}
+      
+      {/**<MiComponent3 nombre="alex" apellidos="Carvajal" ficha= {ficha_m} /> */} 
       </div>
-      <hr/>
        {
        /*<MiComponent/>
        <MiComponent/>
--- src/MiComponent.js
+++ src/MiComponent.js
@@ -1,20 +1,88 @@
 //Crear un componente 
 //Primero se importan los modulos de react /dependencias
-import React  from "react";
 //funcion del component
+import React, {useState, useEffect} from "react";
+import {FiChevronRight, FiChevronLeft} from 'react-icons/fi';
+import { FaQuoteRight } from "react-icons/fa";
+import data from './slider/data';
+import './slider/slider.css'
 
-const MiComponent = () =>{
+function MiComponent(){
+    const [people, setPeople] = useState(data);
+    const [index, setIndex] = React.useState(0);
 
-   /* let nombre = "alex";
+    useEffect(() => {
+        const lastIndex = people.length - 1;
+        if (index < 0) {
+          setIndex(lastIndex);
+        }
+        if (index > lastIndex) {
+          setIndex(0);
+        }
+      }, [index, people]);
+
+      useEffect(() => {
+        let slider = setInterval(() => {
+          setIndex(index + 1);
+        }, 5000);
+        return () => {
+          clearInterval(slider);
+        };
+      }, [index]);
+
+      return (
+        <section className="section">
+          <div className="title">
+            <h2>
+              <span>/</span>reviews
+            </h2>
+          </div>
+          <div className="section-center">
+            {people.map((person, personIndex) => {
+              const { id, image, name, title, quote } = person;
+    
+              let position = 'nextSlide';
+              if (personIndex === index) {
+                position = 'activeSlide';
+              }
+              if (
+                personIndex === index - 1 ||
+                (index === 0 && personIndex === people.length - 1)
+              ) {
+                position = 'lastSlide';
+              }
+    
+              return (
+                <article className={position} key={id}>
+                  <img src={image} alt={name} className="person-img" />
+                  <h4>{name}</h4>
+                  <p className="title">{title}</p>
+                  <p className="text">{quote}</p>
+                  <FaQuoteRight className="icon" />
+                </article>
+              );
+            })}
+            <button className="prev" onClick={() => setIndex(index - 1)}>
+              <FiChevronLeft />
+            </button>
+            <button className="next" onClick={() => setIndex(index + 1)}>
+              <FiChevronRight />
+            </button>
+          </div>
+        </section>
+      );
+    
+
+}
+
+export default MiComponent;
+/* let nombre = "alex";
     let ciudad = "Popayan" 
-    */
    let usuario ={
         nombre: "alex",
         apellidos: "carvajal",
         ciudad: "popayan"
-
     }
-
     return (
         <div className="MiComponent">
     <h2>Componente creado</h2>
@@ -30,9 +98,7 @@ const MiComponent = () =>{
             Ciudad: <strong>{usuario.ciudad}</strong>
         </li>
     </ul>
+  </div>
+    );  */
 
-        </div>
-    );
-}
 //exportar
-export default MiComponent;
