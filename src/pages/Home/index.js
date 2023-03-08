61cc7258f473414738c6b33a74fa7101c8327adf
Alex Carvajal <alexcarvajal@unicauca.edu.co>
2023-03-08 17:31:00 -0500

    arreglo deploy

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,8 +4,6 @@ import Features from "../../components/Features/Features";
 import Growth from "../../components/Growth/Growth";
 import Questions from "../../components/Questions/Questions";
 import Programs from "../../components/Programs/Programs";
-import Footer from "../../components/Footer/Footer";
-import { programs_user } from "../../constants/programs_user";
 import { programs_shopper } from "../../constants/programs_shopper";
 import Slider from "../../components/Slider/Slider"; 
 
